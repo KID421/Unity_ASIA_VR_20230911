b9df2a248bca79d4b981ff4b37f56b7c3803c68c
KID <50666207+KID421@users.noreply.github.com>
2023-10-23 10:53:09 +0800

    Update 生成子彈

--- Assets/Sripts/FireSystem.cs
+++ Assets/Sripts/FireSystem.cs
@@ -2,7 +2,48 @@
 
 public class FireSystem : MonoBehaviour
 {
-    public int magazineCount;
-    public int bulletTotal;
-    public int bulletCurrent;
+    [Header("彈匣的子彈數量"), Range(0, 10)]
+    public int magazineCount = 7;
+    [Header("子彈總數"), Range(20, 200)]
+    public int bulletTotal = 28;
+    [Header("目前子彈數量"), Range(0, 10)]
+    public int bulletCurrent = 7;
+    [Header("子彈預製物")]
+    public GameObject prefabBullet;
+    [Header("子彈生成點")]
+    public Transform pointSpawnBullet;
+
+    public bool openDoor = true;
+    public bool isDead = false;
+
+    private void Awake()
+    {
+        // print("<color=green>喚醒事件</color>");
+        Instantiate(prefabBullet);
+    }
+
+    private void Start()
+    {
+        // print("<color=yellow>開始事件</color>");
+
+        if (openDoor)
+        {
+            print("已經開門");
+        }
+
+        if (isDead)
+        {
+            print("已經死亡");
+        }
+    }
+
+    private void Update()
+    {
+        // print("<color=red>更新事件</color>");
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(prefabBullet);
+        }
+    }
 }
