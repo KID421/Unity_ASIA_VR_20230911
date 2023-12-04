c4b42e547636cfd25f300d544ef0cf281e0148da
KID <50666207+KID421@users.noreply.github.com>
2023-12-04 10:49:38 +0800

    Update 添加子彈碰撞刪除

---
+++ Assets/Sripts/Bullet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // 子彈碰到物件時刪除
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
